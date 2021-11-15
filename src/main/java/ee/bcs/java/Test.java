4c0a68f6bcac3ee54659010daf50a3f366db5168
Siim Rebane <siim@rebane.org>
2021-11-15 19:20:06 +0200

    näidis commit

---
+++ src/main/java/ee/bcs/java/Test.java
@@ -0,0 +1,16 @@
+package ee.bcs.java;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        a = a + b;
+        b  = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
+
