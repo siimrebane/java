73aeae805a9f7e20f028d536dde64f8023431aa2
Siim Rebane <siim@rebane.org>
2022-04-06 18:18:41 +0300

    Lesson0

---
+++ src/main/java/ee/bcs/java/tasks/Lesson0.java
@@ -0,0 +1,16 @@
+package ee.bcs.java.tasks;
+
+public class Lesson0 {
+
+    public static void main(String[] args) {
+        int a = 4;
+
+        // kui a > 5, siis trüki välja "a > 5"
+        // kui a > 4, siis trüki välja "a > 4";
+        // kui a > 3, siis trüki välja "a > 4";
+        // kui a > 2, siis trüki välja "a > 4";
+        // kui a > 1, siis trüki välja "a > 4";
+        // vastasel juhul trüki välja "a on väike number";
+
+    }
+}
