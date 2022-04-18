2393849cb1344e6317941cc34f4f6e9e152a1ab2
Siim Rebane <siim@rebane.org>
2022-04-18 18:32:16 +0300

    lisan puuduvad ülesande kirjeldused

--- src/main/java/ee/bcs/java/tasks/Lesson3.java
+++ src/main/java/ee/bcs/java/tasks/Lesson3.java
@@ -33,10 +33,13 @@ public class Lesson3 {
         return new int[0];
     }
 
+    // TODO liida kokku kõik paaris fibonacci arvud kuni numbrini x
     public static int evenFibonacci(int x){
         return 0;
     }
 
+    // TODO kirjuta programm, mis tagastab sisestatud teksti morse koodis (https://en.wikipedia.org/wiki/Morse_code)
+    // Kasuta sümboleid . ja - ning eralda kõik tähed tühikuga
     public static String morseCode(String text){
         return "";
     }
