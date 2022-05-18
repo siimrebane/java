efcc3e60467866dd33b4eac6bb104f679d6c5c1a
Siim Rebane <siim@rebane.org>
2022-05-18 20:25:22 +0300

    väike muudatus 2

--- src/main/java/ee/bcs/java/Test.java
+++ src/main/java/ee/bcs/java/Test.java
@@ -3,7 +3,7 @@ package ee.bcs.java;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello John");
     }
 
 }
