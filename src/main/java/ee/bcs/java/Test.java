b98cdeda02e38591f4a88e8789be708cef638d26
Siim Rebane <siim@rebane.org>
2022-05-18 20:22:36 +0300

    võtan muudatuse tagasi

--- src/main/java/ee/bcs/java/Test.java
+++ src/main/java/ee/bcs/java/Test.java
@@ -3,13 +3,7 @@ package ee.bcs.java;
 public class Test {
 
     public static void main(String[] args) {
-        int a = 3;
-        int b = 6;
-        a = a + b;
-        b  = a - b;
-        a = a - b;
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println("Hello World");
     }
 
 }
