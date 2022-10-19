57afed8282ea7abf7608d951d0ffe053e6dcb377
Siim Rebane <siim@rebane.org>
2022-10-19 20:04:24 +0300

    Sample bank controller

--- src/main/java/ee/bcs/java/solution/bank/SampleBankService.java
+++ src/main/java/ee/bcs/java/solution/bank/SampleBankService.java
@@ -1,8 +1,5 @@
 package ee.bcs.java.solution.bank;
 
-import ee.bcs.java.repository.Account;
-import ee.bcs.java.repository.AccountJpaRepository;
-import ee.bcs.java.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -16,9 +13,10 @@ public class SampleBankService {
     private NamedParameterJdbcTemplate jdbcTemplate;
 
 
-    public void createAccount(String accountNumber){
+    public void createAccount(String accountNumber) {
         // TODO
     }
+
     public Integer getAccountBalance(String accountNumber) {
         String sql = "SELECT balance FROM account WHERE account_number = :accountNumber";
         Map paramMap = new HashMap();
